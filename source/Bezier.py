e30aad8029cda9e3582496819701e196836e63ad
Naturelist <andreastree@icloud.com>
2026-06-09 09:47:23 +0200

    added full editor

---
+++ source/Bezier.py
@@ -0,0 +1,117 @@
+"""Bezier, a module for creating Bezier curves.
+Version 1.1, from < BezierCurveFunction-v1.ipynb > on 2019-05-02
+"""
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+
+__all__ = ["Bezier"]
+
+class Bezier():
+    def TwoPoints(t, P1, P2):
+        """
+        Returns a point between P1 and P2, parametised by t.
+        INPUTS:
+            t     float/int; a parameterisation.
+            P1    numpy array; a point.
+            P2    numpy array; a point.
+        OUTPUTS:
+            Q1    numpy array; a point.
+        """
+
+        if not isinstance(P1, np.ndarray) or not isinstance(P2, np.ndarray):
+            raise TypeError('Points must be an instance of the numpy.ndarray!')
+        if not isinstance(t, (int, float)):
+            raise TypeError('Parameter t must be an int or float!')
+
+        Q1 = (1 - t) * P1 + t * P2
+        return Q1
+
+    def Points(t, points):
+        """
+        Returns a list of points interpolated by the Bezier process
+        INPUTS:
+            t            float/int; a parameterisation.
+            points       list of numpy arrays; points.
+        OUTPUTS:
+            newpoints    list of numpy arrays; points.
+        """
+        newpoints = []
+        #print("points =", points, "\n")
+        for i1 in range(0, len(points) - 1):
+            #print("i1 =", i1)
+            #print("points[i1] =", points[i1])
+
+            newpoints += [Bezier.TwoPoints(t, points[i1], points[i1 + 1])]
+            #print("newpoints  =", newpoints, "\n")
+        return newpoints
+
+    def Point(t, points):
+        """
+        Returns a point interpolated by the Bezier process
+        INPUTS:
+            t            float/int; a parameterisation.
+            points       list of numpy arrays; points.
+        OUTPUTS:
+            newpoint     numpy array; a point.
+        """
+        newpoints = points
+        #print("newpoints = ", newpoints)
+        while len(newpoints) > 1:
+            newpoints = Bezier.Points(t, newpoints)
+            #print("newpoints in loop = ", newpoints)
+
+        #print("newpoints = ", newpoints)
+        #print("newpoints[0] = ", newpoints[0])
+        return newpoints[0]
+
+    def Curve(t_values, points):
+        """
+        Returns a point interpolated by the Bezier process
+        INPUTS:
+            t_values     list of floats/ints; a parameterisation.
+            points       list of numpy arrays; points.
+        OUTPUTS:
+            curve        list of numpy arrays; points.
+        """
+
+        if not hasattr(t_values, '__iter__'):
+            raise TypeError("`t_values` Must be an iterable of integers or floats, of length greater than 0 .")
+        if len(t_values) < 1:
+            raise TypeError("`t_values` Must be an iterable of integers or floats, of length greater than 0 .")
+        if not isinstance(t_values[0], (int, float)):
+            raise TypeError("`t_values` Must be an iterable of integers or floats, of length greater than 0 .")
+
+        curve = np.array([[0.0] * len(points[0])])
+        for t in t_values:
+            #print("curve                  \n", curve)
+            #print("Bezier.Point(t, points) \n", Bezier.Point(t, points))
+
+            curve = np.append(curve, [Bezier.Point(t, points)], axis=0)
+
+            #print("curve after            \n", curve, "\n--- --- --- --- --- --- ")
+        curve = np.delete(curve, 0, 0)
+        #print("curve final            \n", curve, "\n--- --- --- --- --- --- ")
+        return curve
+
+
+t_points = np.arange(0, 1, 0.01) #................................. Creates an iterable list from 0 to 1.
+points1 = np.array([[0, 0], [0, 8], [5, 10], [9, 7], [4, 3]]) #.... Creates an array of coordinates.
+curve1 = Bezier.Curve(t_points, points1) #......................... Returns an array of coordinates.
+
+plt.figure()
+plt.plot(
+    curve1[:, 0],   # x-coordinates.
+    curve1[:, 1]    # y-coordinates.
+)
+plt.plot(
+    points1[:, 0],  # x-coordinates.
+    points1[:, 1],  # y-coordinates.
+    'ro:'           # Styling (red, circles, dotted).
+)
+plt.grid()
+plt.show()
+
